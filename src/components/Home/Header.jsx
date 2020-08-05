ef6f51b95c15bf45a707927c5b97da3980b91afd
Panchal, Nikunj <panchalnv@users.noreply.github.com>
2020-08-05 19:47:02 -0400

    Commits: 1) Fix Scollbar issue for Home and Contact screens. 2) Use Google drive API to bring the Updated Resume from drive.

--- src/components/Home/Header.jsx
+++ src/components/Home/Header.jsx
@@ -32,7 +32,7 @@ const useStyles = makeStyles((theme) => ({
     top: '50%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
-    width: '100vw',
+    //width: '100vw',
     textAlign: 'center',
     zIndex: 1,
   },
