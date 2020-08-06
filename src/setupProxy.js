bf719f0baf832e6e92ff41a21b9904d7c41b8f71
panchalnv <panchalnv@users.noreply.github.com>
2020-08-06 19:48:14 -0400

    Update setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,11 +2,14 @@ const { createProxyMiddleware } = require('http-proxy-middleware')
 
 module.exports = function (app) {
   app.use(
-    createProxyMiddleware({
-      target: 'https://drive.google.com/',
+    createProxyMiddleware('/uc', {
+      target: 'https://drive.google.com',
       changeOrigin: true,
       //ws: true, // proxy websockets
       //pathRewrite: {'^/' : ''}
+      router: {
+        'https://panchal.netlify.app/': 'https://drive.google.com'
+      }
     }),
   );
 };
