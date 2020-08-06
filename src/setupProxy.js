356db9b4e5e8b16741d1b3e3eea8c6e68ebb46ca
panchalnv <panchalnv@users.noreply.github.com>
2020-08-06 19:38:26 -0400

    Update setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,11 +2,11 @@ const { createProxyMiddleware } = require('http-proxy-middleware')
 
 module.exports = function (app) {
   app.use(
-    createProxyMiddleware('/uc', {
+    createProxyMiddleware('/', {
       target: 'https://drive.google.com',
       changeOrigin: true,
       //ws: true, // proxy websockets
       pathRewrite: {'^/' : ''}
-    }),    
+    }),
   );
 };
